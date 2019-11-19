891410b23350d3cb6f5f33a3827bbf37fd06649a
Eric Pederson <epederson@denniskirk.com>
2019-11-19 17:08:03 -0600

    example implementation

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -13,21 +13,52 @@ export class FizzBuzz extends LitElement {
   static get properties() {
     return {
       counter: { type: Number },
+      output: { type: String },
+      fizz: { type: Number },
+      buzz: { type: Number }
     };
   }
 
   constructor() {
     super();
     this.counter = 1;
+    this.fizz = 3;
+    this.buzz = 5;
+    this._setOutput()
   }
 
   __increment() {
     this.counter += 1;
+    this._setOutput();
+  }
+
+  _setOutput() {
+    if (this._isFizzBuzz()) {
+        this.output = "FizzBuzz";
+    } else if (this._isFizz()) {
+        this.output = "Fizz";
+    } else if (this._isBuzz()) {
+        this.output = "Buzz";
+    } else {
+        this.output = this.counter;
+    }
+  }
+
+  _isFizz(){
+    return (this.counter % this.fizz) === 0;
+  }
+
+   _isBuzz(){
+    return this.counter % this.buzz === 0;
+  }
+
+  _isFizzBuzz(){
+    return this._isFizz() && this._isBuzz();
   }
 
   render() {
     return html`
-      <h2>Fizz Buzz exercise Nr. ${this.counter}!</h2>
+      <h2>Fizz Buzz exercise Nr. ${this.output}!</h2>
       <button @click=${this.__increment}>increment</button>
     `;
   }
